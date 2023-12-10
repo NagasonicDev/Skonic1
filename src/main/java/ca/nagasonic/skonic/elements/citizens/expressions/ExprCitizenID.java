1854be43d53709924f4a95b02c9d75a8d46fffcf
NagasonicDev <nagasonicdev@gmail.com>
2023-12-10 18:22:51 +1100

    Initial Commit

--- src/main/java/ca/nagasonic/skonic/elements/citizens/expressions/ExprCitizenID.java
+++ src/main/java/ca/nagasonic/skonic/elements/citizens/expressions/ExprCitizenID.java
@@ -19,6 +19,7 @@ public class ExprCitizenID extends SimpleExpression<Number> {
     @Override
     protected @Nullable Number[] get(Event e) {
         if (npc != null && npc.getSingle(e) != null){
+            if ((Integer) npc.getSingle(e).getId() == null) return null;
             return new Number[]{npc.getSingle(e).getId()};
         }else return null;
     }
